5cb7f64414d5ec52f8c430b86e53920ecbaf22b1
HerrisSuhendra <herris.pm@gmail.com>
2018-11-26 20:59:43 +0700

    remove depedency text2png

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var util = require('./util')
 var fs = require('fs');
-var text2png = require('text2png');
 var wrap = require('word-wrap');
 var receipt = require('receipt')
 var _ = require('underscore')
@@ -55,62 +54,62 @@ router.get('/printfile/:printerName/:fileName', async (req, res) => {
     res.send(400)
   }
 })
-async function printMenu(_transaction, printerName) {
-  receipt.config.currency = '';
-  receipt.config.width = 40;
-  receipt.config.ruler = '_';
-  let header = receipt.create([
-    {
-      type: 'text', value: [
-        `${_transaction.merchant.name}`,
-      ], align: 'center'
-    },
-  ])
-  let table = receipt.create([
-    {
-      type: 'text', value: [
-        `TABLE ${_transaction.tableNo}`,
-      ], align: 'center'
-    },
-  ])
-  let _listTransactions = []
-  _transaction.ListTransactionDetail.forEach((e) => {
-    let _detail = {}
-    _detail.item = e.listing.name
-    _detail.qty = e.qty
-    _detail.cost = e.price
-    _detail.remark = e.remark
-    if (e.discount) {
-      _detail.discount = { type: 'absolute', value: e.discount }
-    }
-    _listTransactions.push(_detail)
-  });
-  let list = receipt.create([
-    { type: 'tableMenu', lines: _listTransactions }
-  ])
-  fs.writeFileSync('menu.png',
-    text2png(`${header}\nCustomer  : ${_transaction.user.fullname}\nTelp          : ${_transaction.user.phone}\n${table}\n${list}`, {
-      font: '15px Arial Black',
-      color: 'black',
-      backgroundColor: 'white',
-      lineSpacing: 10,
-      paddingLeft: 25,
-      paddingRight: 25,
-      paddingBottom: 40,
-      paddingTop: 20
-    })
-  );
-  try {
-    if (process.platform == 'darwin' || process.platform == 'linux') {
-      await util.printFileMac(printerName, 'menu.png')
-    } else {
-      await util.printFileWindows(printerName, 'menu.png')
-    }
-  } catch (error) {
-    console.log(error)
-  }
-  return true
-}
+// async function printMenu(_transaction, printerName) {
+//   receipt.config.currency = '';
+//   receipt.config.width = 40;
+//   receipt.config.ruler = '_';
+//   let header = receipt.create([
+//     {
+//       type: 'text', value: [
+//         `${_transaction.merchant.name}`,
+//       ], align: 'center'
+//     },
+//   ])
+//   let table = receipt.create([
+//     {
+//       type: 'text', value: [
+//         `TABLE ${_transaction.tableNo}`,
+//       ], align: 'center'
+//     },
+//   ])
+//   let _listTransactions = []
+//   _transaction.ListTransactionDetail.forEach((e) => {
+//     let _detail = {}
+//     _detail.item = e.listing.name
+//     _detail.qty = e.qty
+//     _detail.cost = e.price
+//     _detail.remark = e.remark
+//     if (e.discount) {
+//       _detail.discount = { type: 'absolute', value: e.discount }
+//     }
+//     _listTransactions.push(_detail)
+//   });
+//   let list = receipt.create([
+//     { type: 'tableMenu', lines: _listTransactions }
+//   ])
+//   fs.writeFileSync('menu.png',
+//     text2png(`${header}\nCustomer  : ${_transaction.user.fullname}\nTelp          : ${_transaction.user.phone}\n${table}\n${list}`, {
+//       font: '15px Arial Black',
+//       color: 'black',
+//       backgroundColor: 'white',
+//       lineSpacing: 10,
+//       paddingLeft: 25,
+//       paddingRight: 25,
+//       paddingBottom: 40,
+//       paddingTop: 20
+//     })
+//   );
+//   try {
+//     if (process.platform == 'darwin' || process.platform == 'linux') {
+//       await util.printFileMac(printerName, 'menu.png')
+//     } else {
+//       await util.printFileWindows(printerName, 'menu.png')
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+//   return true
+// }
 router.post('/printMenu', async (req, res) => {
   const _transaction = req.body.data
   //beverage
